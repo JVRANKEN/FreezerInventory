c3190f3cbf2abbdd8a9fdedc192864992ed8a40c
Jeroen Vranken <jeroen.vranken1993@gmail.com>
2023-12-06 00:21:28 +0100

    13-frontendsetup: add primeNG with theme selector

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {ThemeService} from "./theme.service";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private themeService: ThemeService) {}
+
   title = 'frontend';
+  selectedState: any = null;
+
+  states: any[] = [
+    {name: 'Arizona', code: 'Arizona'},
+    {name: 'California', value: 'California'},
+    {name: 'Florida', code: 'Florida'},
+    {name: 'Ohio', code: 'Ohio'},
+    {name: 'Washington', code: 'Washington'}
+  ];
+
+  cities1: any[] = [];
+
+  cities2: any[] = [];
+
+  city1:any = null;
+
+  city2:any = null;
+
+  changeTheme(theme: string) {
+    this.themeService.switchTheme(theme);
+  }
 }
