80eb3076ede2802d85f1f9729181ac4d84039341
Jeroen Vranken <jeroen.vranken1993@gmail.com>
2024-01-09 20:05:09 +0100

    21-routing: add datePipe

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
+import {DatePipe} from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
   provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    DatePipe
   ]
 };
